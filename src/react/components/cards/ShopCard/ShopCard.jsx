b65c051ad78dbf0d6ba7899f70587dfe873b22e0
fldNoar <fld.noar@gmail.com>
2025-03-31 16:30:42 +0300

    Fix styles of Cards and remove Navigate component

--- src/react/components/cards/ShopCard/ShopCard.jsx
+++ src/react/components/cards/ShopCard/ShopCard.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import styles from './ShopCard.module.scss';
 
-const ShopCard = ({title, description, price}) => {
+const ShopCard = ({title, description, price, image}) => {
+
+    const slicedTitle = title.slice(0, 30);
+
     return (
         <article className={styles.shopCard}>
-            <img className={styles.shopCard__img} src="https://png.pngtree.com/thumb_back/fw800/background/20230610/pngtree-picture-of-a-blue-bird-on-a-black-background-image_2937385.jpg" alt="image"/>
-            <h2 className={styles.shopCard__title}>{title}</h2>
+            <img className={styles.shopCard__img} src={image} alt="image"/>
+            <h2 className={styles.shopCard__title}>{slicedTitle}</h2>
             <p className={styles.shopCard__descr}>{description}</p>
             <span className={styles.shopCard__price}>Price: {price}$</span>
         </article>
--- src/react/sections/CardsList/CardsList.jsx
+++ src/react/sections/CardsList/CardsList.jsx
@@ -1,24 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import ShopCard from "../../components/cards/ShopCard/ShopCard";
 import styles from "./CardsList.module.scss";
 
-const CardsList = () => {
-
-    const [cards, setCards] = useState([
-        {id: 1, title: 'Card 1', description: 'Lorem ipsum dolor sit amet consectetur. Ultrices ut quam ornare hac et phasellus suspendisse pellentesque velit.', price: 9.99},
-        {id: 2, title: 'Card 2', description: 'Lorem ipsum dolor sit amet consectetur. Ultrices ut quam ornare hac et phasellus suspendisse pellentesque velit.', price: 4.99},
-        {id: 3, title: 'Card 3', description: 'Lorem ipsum dolor sit amet consectetur. Ultrices ut quam ornare hac et phasellus suspendisse pellentesque velit.', price: 2.99},
-        {id: 4, title: 'Card 4', description: 'Lorem ipsum dolor sit amet consectetur. Ultrices ut quam ornare hac et phasellus suspendisse pellentesque velit.', price: 11.99},
-        {id: 5, title: 'Card 5', description: 'Lorem ipsum dolor sit amet consectetur. Ultrices ut quam ornare hac et phasellus suspendisse pellentesque velit.', price: 5.99},
-        {id: 6, title: 'Card 6', description: 'Lorem ipsum dolor sit amet consectetur. Ultrices ut quam ornare hac et phasellus suspendisse pellentesque velit.', price: 1.99},
-    ]);
-
-    const [selectedCard, setSelectedCard] = useState('');
-
+const CardsList = ({cards}) => {
     return (
         <section className={styles.cardsContainer}>
             {cards.map((card, index) => (
-                <ShopCard key={index} title={card.title} description={card.description} price={card.price} />
+                <ShopCard key={index} title={card.title} description={card.description} price={card.price} image={card.images[0]} />
             ))}
         </section>
     );
